cb31b8ac39878fe0be92ec3077aa6e1a84125b5f
IsraelChidera <israelchidera54@gmail.com>
2022-12-02 02:05:12 +0100

    Adding project...

--- MusicApp/Songs.cs
+++ MusicApp/Songs.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MusicApp
+﻿namespace MusicApp
 {
     internal class Songs
     {
@@ -45,68 +39,65 @@ namespace MusicApp
                 Console.WriteLine($"- \t -- \t{song.SongName}");
             }
 
+            Console.WriteLine("\nMusic section\nWhat do you want to do?");
+            Console.WriteLine("\tTo play all songs, press 1\n\tTo add song, press 2\n\tTo exit, press 0\n");
+            Console.Write("====>");
+            string musicOption = Console.ReadLine();
+
             bool isTrue = true;
             while (isTrue)
             {
-                try
+
+
+               
+                Songs songsList = new();
+
+                switch (musicOption)
                 {
-
-                Start: Console.WriteLine("\nMusic section\nWhat do you want to do?");
-                    Console.WriteLine("\tTo play all songs, press 1\n\tTo add song, press 2\n\tTo exit, press 0\n");
-                    Console.Write("====>");
-                    string musicOption = Console.ReadLine();
-                    Songs songsList = new();
-
-                    switch (musicOption)
-                    {
-                        case "1":
-                            Console.Clear();
-                            Console.WriteLine("Playing all songs");
-                            foreach (var song in songCollection)
-                            {
-                                Console.WriteLine($"Now playing\n- \t-- \t{song.SongName}");
-                                Thread.Sleep(2000);
-                            }
-                            goto Start;
-                        case "2":
-                            Console.Clear();
-                            Console.WriteLine("Adding songs");
-                            Console.WriteLine("\nAdd new song");
-                            Console.Write("song ====>");
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Playing all songs");
+                        foreach (var song in songCollection)
+                        {
+                            Console.WriteLine($"Now playing\n- \t-- \t{song.SongName}");
+                            Thread.Sleep(2000);
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Adding songs");
+                        Console.WriteLine("\nAdd new song");
+                        Console.Write("song ====>");
 
 
-                            songsList.SongName = Console.ReadLine();
-                            songCollection.Add(new Songs() { SongName = songsList.SongName });
-                            Console.WriteLine("You've added a song...\n");
+                        songsList.SongName = Console.ReadLine();
+                        songCollection.Add(new Songs() { SongName = songsList.SongName });
+                        Console.WriteLine("You've added a song...\n");
 
-                            Console.WriteLine("\nAll songs");
-                            foreach (var songs in songCollection)
-                            {
-                                Console.WriteLine($"\n\t{songs.SongName}");
-                            }
-                            //isTrue = false;
+                        Console.WriteLine("\nAll songs");
+                        foreach (var songs in songCollection)
+                        {
+                            Console.WriteLine($"\n\t{songs.SongName}");
+                        }
+                        //isTrue = false;
 
-                            goto Start;
-                        case "0":
-                            Console.Clear();
-                            Console.WriteLine("\nExiting song list");                           
-                            break;
-                        default:
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("\nPlease provide a valid input\n");
-                            Console.ResetColor();
-                            DisplayMusicList();
-                            goto Start;
-                    }
+                        break;
+                    case "0":
+                        Console.Clear();
+                        Console.WriteLine("\nExiting song list");
+                        return;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nPlease provide a valid input\n");
+                        Console.ResetColor();                        
+                        break;
                 }
-                catch (ArgumentOutOfRangeException e)
-                {
-                    Console.WriteLine("Invalid input");
-                }
-                catch (FormatException ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                }
+
+                Console.WriteLine("\nMusic section\nWhat do you want to do?");
+                Console.WriteLine("\tTo play all songs, press 1\n\tTo add song, press 2\n\tTo exit, press 0\n");
+                Console.Write("====>");
+                musicOption = Console.ReadLine();
+
             }
 
 
